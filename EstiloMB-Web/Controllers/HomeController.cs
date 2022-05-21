379c4f483f59356d3371012150b07adf08295af7
Jhon Everton <48407983+jhonApp@users.noreply.github.com>
2022-05-21 18:44:58 -0300

    Tela de detalhes

--- EstiloMB-Web/Controllers/HomeController.cs
+++ EstiloMB-Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EstiloMB_Web.Controllers
+namespace EstiloMB.Site.Controllers
 {
     public class HomeController : Controller
     {
---
+++ EstiloMB-Web/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EstiloMB.Site.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
