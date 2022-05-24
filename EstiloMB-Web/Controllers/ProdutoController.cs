cecb17ce4b01f1f90836b09b1264adc33a48c616
Jhon Everton <48407983+jhonApp@users.noreply.github.com>
2022-05-23 22:22:06 -0300

    organizendo projeto

--- EstiloMB-Web/Controllers/ProdutoController.cs
+++ EstiloMB-Web/Controllers/ProdutoController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EstiloMB.Site.Controllers
 {
     public class ProdutoController : Controller
     {
+        [Authorize]
         public IActionResult Details()
         {
             return View();
