f0f6e2602cdb9d6c3ab833badbca6c7e796665bf
Jhon Everton <jhonevertonapp@outlook.com>
2023-01-30 17:43:10 -0300

    Details - Tamanho funcionando

---
+++ EstiloMB-Web/Views/Produto/teste.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EstiloMB_Web.Views.Produto
+{
+    public class testeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
